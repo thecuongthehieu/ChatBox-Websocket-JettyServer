6d3a1b9b27617393339b6385cd7085de87dea9a9
cuong.ntt <cuong.ntt@coccoc.com>
2023-04-03 15:37:15 +0700

    Add simple Unit Test

---
+++ src/webchat/src/test/java/com/dev/ChatBoxTest.java
@@ -0,0 +1,39 @@
+package com.dev;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChatBoxTest {
+	private ChatBox chatBox;
+	@BeforeEach
+	public void initChatBox() {
+		this.chatBox = new ChatBox();
+	}
+
+	@Test
+	public void testAddUser() {
+		assertEquals(this.chatBox.getNumberOfClients(), 0);
+		this.chatBox.addUser("user1", new ChatWebSocket());
+		assertEquals(this.chatBox.getNumberOfClients(), 1);
+		this.chatBox.addUser("user2", new ChatWebSocket());
+		assertEquals(this.chatBox.getNumberOfClients(), 2);
+	}
+
+	@Test
+	public void testRemoveUser() {
+		assertEquals(this.chatBox.getNumberOfClients(), 0);
+		ChatWebSocket user1WebSocket = new ChatWebSocket();
+		ChatWebSocket user2WebSocket = new ChatWebSocket();
+		this.chatBox.addUser("user1", user1WebSocket);
+		this.chatBox.addUser("user2", user2WebSocket);
+		assertEquals(this.chatBox.getNumberOfClients(), 2);
+
+		this.chatBox.removeUser(user1WebSocket);
+		assertEquals(this.chatBox.getNumberOfClients(), 1);
+		this.chatBox.removeUser(user2WebSocket);
+		assertEquals(this.chatBox.getNumberOfClients(), 0);
+	}
+}
